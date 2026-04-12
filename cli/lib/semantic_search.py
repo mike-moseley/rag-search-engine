9a680024504c4c85d5c956bb8bc15914d7e537d7
Mike Moseley <mike.moseley@tuta.io>
2026-04-12 02:37:05 -0400

    ...

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -22,7 +22,7 @@ class SemanticSearch:
 
         movie_list = []
         for doc in documents:
-            movie_list.append("f{doc['title']}: {doc['description']}")
+            movie_list.append(f"{doc['title']}: {doc['description']}")
 
         embeddings = self.model.encode(movie_list, show_progress_bar=True)
         self.embeddings = embeddings
@@ -43,6 +43,29 @@ class SemanticSearch:
         self.documents = documents
         for doc in documents:
             self.document_map[doc['id']] = doc
+
+    def search(self, query: str, limit: int) -> list[dict] | None:
+        if self.embeddings is None:
+            raise ValueError("No embeddings loaded. Call `load_or_create_embeddings` first.")
+        if self.documents is None:
+            raise ValueError("No documents loaded. Call `populate_dod_doc_map` first")
+        embedded_query = self.generate_embedding(query)
+        scores: list[tuple] = []
+        for i,e in enumerate(self.embeddings):
+            scores.append((cosine_similarity(e,embedded_query), i))
+
+        scores.sort(key=lambda x:x[0], reverse=True)
+        embedded_dicts: list[dict[str, float | str]] = []
+        for score, i in scores[:limit]:
+            doc = self.documents[i]
+            embedded_dicts.append({
+                "score": score,
+                "title": doc["title"],
+                "description": doc["description"],
+            })
+
+        return embedded_dicts
+
 
 def verify_model():
     semantic_search = SemanticSearch()
@@ -68,3 +91,42 @@ def verify_embeddings():
     print(f"Number of docs: {len(documents)}")
     print(f"Embeddings shape: {embedding.shape[0]} vectors in {embedding.shape[1]} dimensions")
 
+def embed_query_text(query: str):
+    semantic_search = SemanticSearch()
+    print(f"Query: {query}")
+    embedding = semantic_search.generate_embedding(query)
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Shape: {embedding.shape[0]}")
+
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+
+    return dot_product / (norm1 * norm2)
+
+def search_command(query:str, limit:int):
+    semantic_search = SemanticSearch()
+    movies = load_movies()
+    semantic_search.load_or_create_embeddings(movies)
+    results = semantic_search.search(query, limit)
+    if results == None:
+        raise ValueError("Searching failed, results empty")
+    for i,r in enumerate(results, 1):
+        print(f'{i}. {r["title"]} ({r["score"]})\n\t{r["description"][:80]}...')
+
+def chunk_command(query: str, size: int, overlap: int = 0):
+    split = query.split()
+    chunks: list[str] = []
+    step = size - overlap
+    for i in range(0, len(split), step):
+        chunk = " ".join(split[i:i+size])
+        if len(split[i:i+size]) > overlap:
+            chunks.append(chunk)
+
+    print(f"Chunking {len(query)} characters:")
+    for i,chunk in enumerate(chunks, 1):
+        print(f"{i}. {chunk}")
