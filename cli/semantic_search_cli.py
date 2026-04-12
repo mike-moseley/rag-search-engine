57c7b038ab440a4bcf24c2d9e7cf3b7790755738
Mike Moseley <mike.moseley@tuta.io>
2026-04-12 02:37:05 -0400

    ...

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model, embed_text, verify_embeddings
+from lib.semantic_search import embed_query_text, verify_model, embed_text, verify_embeddings, search_command, chunk_command
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="available commands")
 
-    verify_parser = subparsers.add_parser("verify", help="Print the current model and max sequence length of the model")
+    subparsers.add_parser("verify", help="Print the current model and max sequence length of the model")
 
     embed_text_parser = subparsers.add_parser("embed_text", help="Embed text into model")
     embed_text_parser.add_argument("text", help="Text to embed")
 
-    verify_embeddings_parser = subparsers.add_parser("verify_embeddings", help="Verify current embeddings")
+    subparsers.add_parser("verify_embeddings", help="Verify current embeddings")
+
+    embed_query_parser = subparsers.add_parser("embedquery", help="Embed a search query")
+    embed_query_parser.add_argument("query", type=str, help="Query to embed")
+
+    search_parser = subparsers.add_parser("search", help="Returns <limit> results matching <query>")
+    search_parser.add_argument("query", type=str, help="Search query")
+    search_parser.add_argument("--limit", type=int, default=5, required=False, help="Number of results to display")
+
+    chunk_parser = subparsers.add_parser("chunk", help="Chunk data")
+    chunk_parser.add_argument("text", type=str, help="Data to chunk as str")
+    chunk_parser.add_argument("--chunk-size", type=int, default=200, required=False, help="Size in words of each chunk")
+    chunk_parser.add_argument("--overlap", type=int, default=0, required=False, help="Number of words to overlap" )
+
     args = parser.parse_args()
 
     match args.command:
@@ -22,6 +35,13 @@ def main():
             embed_text(args.text)
         case "verify_embeddings":
             verify_embeddings()
+        case "embedquery":
+            embed_query_text(args.query)
+        case "search":
+            search_command(args.query, args.limit)
+        case "chunk":
+            chunk_command(args.text, args.chunk_size, args.overlap)
+
 
         case _:
             parser.print_help()
